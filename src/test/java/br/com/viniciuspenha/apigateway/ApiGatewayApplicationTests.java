1bb052271d3de577eafbd344ea91efc7e6f0e20b
viniciuspenha <penhavinicius@hotmail.com>
2020-12-01 23:21:02 -0300

    initial commit

---
+++ src/test/java/br/com/viniciuspenha/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,12 @@
+package br.com.viniciuspenha.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
